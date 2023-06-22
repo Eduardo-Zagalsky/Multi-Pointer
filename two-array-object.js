983dd2b43157d8b62694dedf6f70d24fb442ea2e
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-06-22 00:48:18 -0500

    added logic to make keys and value pairs

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,7 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let obj = {};
+    for (let i = 0; i < arr1.length; i++) {
+        obj[arr1[i]] = arr2[i] || null;
+    }
+    return obj;
 }
