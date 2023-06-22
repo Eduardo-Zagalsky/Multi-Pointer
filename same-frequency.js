606b782c1a161c6658653ea996b3f5e0faa93ac5
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-06-22 02:23:44 -0500

    added the logic for making nums to string sorting them and comparing them

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,14 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+    let str1 = num1.toString().split("").sort();
+    let str2 = num2.toString().split("").sort();
+    if (str1.length == str2.length) {
+        for (let i = 0; i < str1.length; i++) {
+            console.log(str1[i], str2[i], str1[i] == str2[i]);
+            if (str1[i] != str2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
