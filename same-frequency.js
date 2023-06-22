0d1f3068c57e8dd834e1bdb016e9f1a5aa33a968
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-06-22 03:09:35 -0500

    removed the console.log()

--- same-frequency.js
+++ same-frequency.js
@@ -3,7 +3,6 @@ function sameFrequency(num1, num2) {
     let str2 = num2.toString().split("").sort();
     if (str1.length == str2.length) {
         for (let i = 0; i < str1.length; i++) {
-            console.log(str1[i], str2[i], str1[i] == str2[i]);
             if (str1[i] != str2[i]) {
                 return false;
             }
