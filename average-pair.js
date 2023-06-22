1df5a2dcf8d1e910740b672e300e79c5f00188d9
Eduardo Zagalsky <zagalsky.eduardo@gmail.com>
2023-06-22 00:31:18 -0500

    added the logic for the average, multi pointer.

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,15 @@
-function averagePair(arr,avg) {
-
+function averagePair(arr, num) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let avg = (arr[left] + arr[right]) / 2;
+        if (avg == num) {
+            return true;
+        } else if (avg < num) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
 }
